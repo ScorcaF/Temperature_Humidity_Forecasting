75d7c1c50ffdf11cfbe97e223dd55e283328d526
francesco scorca <70110839+jemjacceit@users.noreply.github.com>
2021-12-07 18:09:09 +0100

    Update WindowGenerator.py

--- WindowGenerator.py
+++ WindowGenerator.py
@@ -1,47 +1,3 @@
-import argparse
-import numpy as np
-import os
-import pandas as pd
-import tensorflow as tf
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--version', type=str, required=True)
-args = parser.parse_args()
-
-
-seed = 42
-tf.random.set_seed(seed)
-np.random.seed(seed)
-
-zip_path = tf.keras.utils.get_file(
-    origin='https://storage.googleapis.com/tensorflow/tf-keras-datasets/jena_climate_2009_2016.csv.zip',
-    fname='jena_climate_2009_2016.csv.zip',
-    extract=True,
-    cache_dir='.', cache_subdir='data')
-csv_path, _ = os.path.splitext(zip_path)
-df = pd.read_csv(csv_path)
-
-column_indices = [2, 5]
-columns = df.columns[column_indices]
-data = df[columns].values.astype(np.float32)
-
-n = len(data)
-train_data = data[0:int(n*0.7)]
-val_data = data[int(n*0.7):int(n*0.9)]
-test_data = data[int(n*0.9):]
-
-mean = train_data.mean(axis=0)
-std = train_data.std(axis=0)
-
-input_width = 6
-
-if args.version == "a":
-    output_width = 3
-elif args.version == "b":
-    output_width = 9
-   
-
 
 class WindowGenerator:
     def __init__(self, input_width, output_width, output_mean, std):
@@ -85,7 +41,4 @@ class WindowGenerator:
         return ds
 
 
-generator = WindowGenerator(input_width, output_width, mean, std)
-train_ds = generator.make_dataset(train_data, True)
-val_ds = generator.make_dataset(val_data, False)
-test_ds = generator.make_dataset(test_data, False)
+
