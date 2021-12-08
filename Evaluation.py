13c1f225a7481724a96f008fcd8b838de5faaffe
francesco scorca <70110839+jemjacceit@users.noreply.github.com>
2021-12-08 16:27:07 +0100

    Update Evaluation.py

--- Evaluation.py
+++ Evaluation.py
@@ -6,17 +6,21 @@ import argparse
 # parser.add_argument('--version', type=str, required=True)
 # args = parser.parse_args()
 
-tensor_specs = (tf.TensorSpec([None, 6, 2], dtype=tf.float32), tf.TensorSpec([None, 2]))
+#Loading test dataset
+tensor_specs = (tf.TensorSpec([None, 6, 2], dtype=tf.float32), tf.TensorSpec([None,9, 2]))
 test_ds = tf.data.experimental.load('./th_test', tensor_specs) 
 test_ds = test_ds.unbatch().batch(1)
 
+#Loading tflite model
 filename = 'model.tflite'
-
 interpreter = tflite.Interpreter(filename)
+
+#Preparing for inference
 interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
+#Predict test samples
 y_true = []
 y_pred = []
 for series in test_ds:
@@ -27,11 +31,19 @@ for series in test_ds:
     
     my_output = interpreter.get_tensor(output_details[0]['index'])
     y_pred.append(my_output[0])
+
     
     
-mae = multi_outputMAE(y_true, y_pred)
+#Evaluating performance 
+temp, hum = multi_outputMAE(y_true, y_pred)
+size = os.path.getsize(filename) / 2.**10
 
 
+#Printing results  
+print("Temperature mae on test set: {:0.03}.\nHumidity mae on test set: {:0.03}.\n Model size: {:} kB.".\
+      format(temp, hum, size))
+
+#Defining metric
 def multi_outputMAE(y_true, y_pred):
     y_true = np.array(y_true).squeeze()
     y_pred = np.array(y_pred).squeeze()
