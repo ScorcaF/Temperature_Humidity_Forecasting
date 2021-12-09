f06c0c049c4a10cf8292e29c918f7ec0169f93bb
francesco scorca <70110839+jemjacceit@users.noreply.github.com>
2021-12-09 13:08:03 +0100

    Update CustomEarlyStopping.py

--- CustomEarlyStopping.py
+++ CustomEarlyStopping.py
@@ -1,10 +1,7 @@
 import tensorflow as tf
 class CustomEarlyStopping(tf.keras.callbacks.Callback):
-  def on_train_begin(self, logs=None):
-    self.num_epochs = 0
-    
+   
   def on_epoch_end(self, epoch, logs):
-    self.num_epochs +=1
-    if logs['val_multi_outputMAE'][0] < 0.7 and logs['val_multi_outputMAE'][1] < 2.5 and self.num_epochs >=15:
+    if logs['val_multi_outputMAE'][0] < 0.7 and logs['val_multi_outputMAE'][1] < 2.5 and epoch >=15:
       print("\nEarly stopping conditions satisfied")
       self.model.stop_training = True
